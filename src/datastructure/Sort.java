57f089fa5cacbf40d47fd1fcde2f6cb7b7414c4f
Kanglai Qian <qiankanglai@gmail.com>
2014-06-09 10:37:17 +0800

    RadixSort烦死了-。-

---
+++ src/datastructure/Sort.java
@@ -0,0 +1,63 @@
+package datastructure;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * Created by Anthony on 2014/6/9.
+ */
+public class Sort {
+    public static int[] RadixSort(int[] arr){
+        int length = arr.length;
+        int max = Math.abs(arr[0]);
+        for(int i = 1; i < arr.length; i++){
+            int t = Math.abs(arr[i]);
+            if(max < t) max = t;
+        }
+        max = Math.max(max, 3); //ensures base >= 2;
+        int base = (int) Math.ceil(Math.sqrt(max));
+        int bucket[] = new int[base*2];
+        int arr2[]  = new int[arr.length];
+        int exp = 1;
+        while(exp > 0 && exp <= max){
+            Arrays.fill(bucket, 0);
+            for(int i = 0; i < arr.length; i++){
+                int t = (arr[i] / exp) % base+base;
+                bucket[t] ++;
+            }
+            for (int i = 1; i < base*2; i++) {
+                bucket[i] += bucket[i - 1];
+            }
+            for(int i = arr.length-1; i >= 0; i--){
+                int t = (arr[i] / exp) % base+base;
+                arr2[--bucket[t]] = arr[i];
+            }
+
+            int []temp = arr;
+            arr = arr2;
+            arr2 = temp;
+
+            exp *= base;    //it may overflow here...
+        }
+        return arr;
+    }
+
+    public static void main(String args[]){
+        final int len = 10;
+        int arr[] = new int[len];
+        Random rand = new Random(0);
+        for(int i = 0; i < len; i++)
+            arr[i] = rand.nextInt();
+        for(int i = 0; i < len; i++) {
+            System.out.print(arr[i]);
+            System.out.print('\t');
+        }
+        System.out.println();
+        arr = RadixSort(arr);
+        for(int i = 0; i < len; i++) {
+            System.out.print(arr[i]);
+            System.out.print('\t');
+        }
+        System.out.println();
+    }
+}
---
+++ src/leetcode/LongestConsecutiveSequence.java
@@ -0,0 +1,31 @@
+package leetcode;
+
+import datastructure.Sort;
+
+/**
+ * Created by Anthony on 2014/6/8.
+ */
+public class LongestConsecutiveSequence {
+    public int longestConsecutive(int[] num) {
+        if(num == null || num.length == 0)
+            return 0;
+        num = Sort.RadixSort(num);
+        int max = 1;
+        int count = 1;
+        for(int i = 1; i < num.length; i++){
+            if(num[i] == num[i-1]+1){
+                count ++;
+                if(count > max)
+                    max = count;
+            }
+            else if(num[i] != num[i-1])
+                count = 1;
+        }
+        return max;
+    }
+
+    public static void main(String args[]){
+        //System.out.println(new LongestConsecutiveSequence().longestConsecutive(new int[]{2147483646,-2147483647,0,2,2147483644,-2147483645,2147483645}));
+        System.out.println(new LongestConsecutiveSequence().longestConsecutive(new int[]{9,1,4,7,3,-1,0,5,8,-1,6}));
+    }
+}
