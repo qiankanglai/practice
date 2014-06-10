531dd3a961381db0efc2326708ae67a6a76969c4
Kanglai Qian <qiankanglai@gmail.com>
2014-06-10 10:56:19 +0800

    有点蛋疼，用栈！建模还是不够

--- src/leetcode/LargestRectangleinHistogram.java
+++ src/leetcode/LargestRectangleinHistogram.java
@@ -1,56 +1,47 @@
 package leetcode;
 
+import java.util.Stack;
+
 /**
  * Created by Anthony on 2014/6/9.
  */
 public class LargestRectangleinHistogram {
+    //http://www.geeksforgeeks.org/largest-rectangle-under-histogram/
+    //用栈来保存！只要维护若干个比较小的bar的位置就行了，比栈顶的元素都不用维护的~用不着
     public int largestRectangleArea(int[] height) {
         if(height == null || height.length == 0)
             return 0;
         int left[] = new int[height.length];
         int right[] = new int[height.length];
 
-        left[0] = 0;
-        for(int i = 1; i < height.length; i++){
-            if(height[i] > height[i-1])
-                left[i] = i;
-            else{
-                int k = i-1;
-                while(k > 0){
-                    if(height[k] > height[i]){
-                        k--;
-                    }
-                    else if(height[k] == height[i]){
-                        k = left[k]-1;
-                        break;
-                    }
-                    else{
-                        break;
-                    }
-                }
-                left[i] = k+1;
+        Stack<Integer> stack = new Stack<Integer>();
+        for(int i = 0; i < height.length; i++){
+            while(!stack.empty()){
+                if(height[stack.peek()] >= height[i])
+                    stack.pop();
+                else
+                    break;
             }
+            if(stack.empty())
+                left[i] = 0;
+            else
+                left[i] = stack.peek()+1;
+            stack.add(i);
         }
-        right[height.length-1] = height.length-1;
-        for(int i = height.length-2; i >= 0; i--){
-            if(height[i] > height[i+1])
-                right[i] = i;
-            else{
-                int k = i+1;
-                while(k < height.length){
-                    if(height[k] > height[i]){
-                        k++;
-                    }
-                    else if(height[k] == height[i]){
-                        k = right[k]+1;
-                        break;
-                    }
-                    else {
-                        break;
-                    }
-                }
-                right[i] = k-1;
+
+        stack = new Stack<Integer>();
+        for(int i = height.length-1; i >= 0; i--){
+            while(!stack.empty()){
+                if(height[stack.peek()] >= height[i])
+                    stack.pop();
+                else
+                    break;
             }
+            if(stack.empty())
+                right[i] = height.length-1;
+            else
+                right[i] = stack.peek()-1;
+            stack.add(i);
         }
 
         int max = 0;
@@ -78,6 +69,7 @@ public class LargestRectangleinHistogram {
     }
 
     public static void main(String args[]){
-        System.out.println(new LargestRectangleinHistogram().largestRectangleArea(new int[]{2,1,5,6,2,3}));
+        //System.out.println(new LargestRectangleinHistogram().largestRectangleArea(new int[]{2,1,5,6,2,3}));
+        System.out.println(new LargestRectangleinHistogram().largestRectangleArea(new int[]{1,1}));
     }
 }
