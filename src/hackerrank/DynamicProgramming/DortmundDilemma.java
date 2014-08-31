5a29b750f25366107b2885200a205b10723b2277
Kanglai Qian <qiankanglai@gmail.com>
2014-09-01 00:02:00 +0800

    重写了新定义solve，外面还是有点不对

--- src/hackerrank/DynamicProgramming/DortmundDilemma.java
+++ src/hackerrank/DynamicProgramming/DortmundDilemma.java
@@ -7,18 +7,10 @@ import java.util.Scanner;
  */
 public class DortmundDilemma {
     //https://www.hackerrank.com/challenges/dortmund-dilemma
-    private static long A[][] = new long[27][27];
     private static long C[][] = new long[27][27];
-    private static long count_cache[][][] = new long[100001][27][27];
     private static final long modulo = 1000000009;
 
     public static void main(String[] args) {
-        for(int i = 1; i <= 26; i++){
-            A[i][0] = 1;
-            for(int j = 1; j <= i; j++){
-                A[i][j] = A[i][j-1] * (i-j+1) % modulo;
-            }
-        }
         for(int i = 1; i <= 26; i++){
             C[i][0] = 1;
             for(int j = 1; j <= i; j++){
@@ -26,73 +18,57 @@ public class DortmundDilemma {
             }
         }
 
-        //组成长度为n的串，其中k1个必须至少出现一次，k2随意
-        for(int k2 = 0; k2 < 27; k2++){
-            count_cache[0][0][k2] = 1;
-        }
-        for(int i = 1; i < 100001; i++){
-            for(int k1 = 0; k1 < 27; k1++){
-                for(int k2 = 0; k2 < 27-k1; k2++){
-                    if(k1 > i){
-                        count_cache[i][k1][k2] = 0;
-                        continue;
-                    }
-                    else if(k1 == i){
-                        count_cache[i][k1][k2] = A[k1][k1];
-                        continue;
-                    }
-
-                    long sum = 0;
-                    if(k1 > 0) {
-                        sum += k1 * count_cache[i - 1][k1 - 1][k2 + 1] % modulo;
-                    }
-                    if(k2 > 0) {
-                        sum += k2 * count_cache[i - 1][k1][k2] % modulo;
-                    }
-                    sum = sum % modulo;
-                    count_cache[i][k1][k2] = sum;
-                }
-            }
-        }
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
         for (int _t = 0; _t < t; _t++) {
             int n = in.nextInt();
             int k = in.nextInt();
-            long sum = solve(n, k);
+            long sum = solve(n, k) - solve(n, k-1)*k;
+            sum = sum % modulo;
             sum *= C[26][k];
             sum = sum % modulo;
             System.out.println(sum);
         }
         in.close();
     }
+    /*
+    public static long pow(int k, int i){
+        long t = 1;
+        for(int j = 0; j < i; j++){
+            t *= k;
+        }
+        return t;
+    }
 
-    private static long solve_cache[][] = new long[100001][27];
-    private static long solve(int n, int k) {        //获得所有全排列
-        if(n <= k)
+    private static long solve(int n, int k) {
+        if(n <= 1)
             return 0;
-        if(solve_cache[n][k] > 0)
-            return solve_cache[n][k];
         long sum = 0;
         for(int i = 1; i <= n/2; i++){
-            for(int _k = 1; _k <= Math.min(i, k); _k++){     //用_k元素构造prefix
-                long t0 = C[k][_k];
-                long t1 = count_cache[i][_k][0] - solve(i, _k);   //组成prefix/suffix，自身不重复
-                if(t1 == 0)
-                    continue;
-                long t2 = 0;
-                if(n == 2*i){
-                    if(_k == k){
-                        t2 = 1;
-                    }
-                }
-                else {
-                    t2 = count_cache[n-2*i][k-_k][_k];
-                }
-                sum += (t0*t1*t2) % modulo;
-            }
+            sum  += (pow(k, i) - solve(i, k)) * pow(k, n-2*i);
         }
-        solve_cache[n][k] = sum;
         return sum;
+    }
+    */
+
+    private static long solve(int n, int k) {
+        if (n <= 1)
+            return 0;
+        long pow_cache[] = new long[n+1];
+        pow_cache[0] = 1;
+        for(int i = 1; i <= n; i++){
+            pow_cache[i] = (k * pow_cache[i-1]) % modulo;
+        }
+        long solve_cache[] = new long[Math.max(n+1, 2)];
+        solve_cache[0] = 0;
+        solve_cache[1] = 0;
+        for(int _n = 2; _n <= n; _n++){
+            long sum = 0;
+            for(int i = 1; i <= _n/2; i++){
+                sum  += ((pow_cache[i] - solve_cache[i]) * pow_cache[_n-2*i]) % modulo;
+            }
+            solve_cache[_n] = sum % modulo;
+        }
+        return solve_cache[n];
     }
 }
